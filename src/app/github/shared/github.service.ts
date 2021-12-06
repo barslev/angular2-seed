68d15314ca307f9bf806608b289daffa3f02b486
jsfix.live analysis <info@coana.tech>
2021-12-06 10:26:33 +0100

    JSFIX patches

--- src/app/github/shared/github.service.ts
+++ src/app/github/shared/github.service.ts
@@ -1,6 +1,6 @@
+import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { Http, URLSearchParams } from '@angular/http';
-import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GithubService {
@@ -23,7 +23,6 @@ export class GithubService {
     params.set('per_page', '100');
 
     let url = `https://api.github.com/${ path }`;
-    return this.http.get(url, {search: params})
-      .map((res) => res.json());
+    return this.http.get(url, {search: params}).pipe(map((res) => res.json()));
   }
 }
